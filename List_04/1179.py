55844b3b513d17c0a8b2c74472cb7b52beaa03d4
Ana Beatriz <105469302+ol-anaa@users.noreply.github.com>
2025-10-25 15:52:42 -0300

    finalziando

---
+++ List_04/1179.py
@@ -0,0 +1,39 @@
+'''
+Neste problema você deverá ler 15 valores colocá-los em 2 vetores conforme estes valores forem pares ou ímpares. 
+Só que o tamanho de cada um dos dois vetores é de 5 posições. Então, cada vez que um dos dois vetores encher,
+você deverá imprimir todo o vetor e utilizá-lo novamente para os próximos números que forem lidos. Terminada 
+a leitura, deve-se imprimir o conteúdo que restou em cada um dos dois vetores, imprimindo primeiro os valores 
+do vetor impar. Cada vetor pode ser preenchido tantas vezes quantas for necessário.
+
+Entrada
+A entrada contém 15 números inteiros.
+
+Saída
+Imprima a saída conforme o exemplo abaixo.
+'''
+
+
+par = []
+impar = []
+
+for num in range(15): 
+    entrada = int(input())
+
+    if len(impar) == 5:
+        for i, p in enumerate(impar):
+            print(f'impar[{i}] = {p}')
+        impar.clear()
+    if len(par) == 5: 
+        for io, po in enumerate(par):
+            print(f'par[{io}] = {po}')
+        par.clear()
+
+    if entrada % 2 == 0:
+        par.append(entrada)
+    else:
+        impar.append(entrada)
+
+for f, g in enumerate(impar):
+    print(f'impar[{f}] = {g}') 
+for h, j in enumerate(par):
+    print(f'par[{h}] = {j}')
